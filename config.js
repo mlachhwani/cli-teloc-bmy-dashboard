73c9b5d2cafdc5e067d69a02dabb2d5e39bd2757
MUKESH LACHHWANI <123182658+mlachhwani@users.noreply.github.com>
2026-01-05 14:59:54 +0530

    Update config.js

--- config.js
+++ config.js
@@ -9,5 +9,5 @@
 */
 
 const API_URL =
-  "https://script.google.com/macros/s/AKfycbwIo55We65opFhRl4RVt-WxRooi5pXutd-xf2nSb_-iq36td864o9thqMu5L7ELBCX11g/exec?api=1";
+  "https://script.google.com/macros/s/AKfycbwlRxPC2jtkBfmRfVJB9CNDYznP61Id9vNYVyXfihEkZF_Qykdlo3UHu5fzJWlqeNFV_g/exec?api=1";
 
